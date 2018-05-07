6648f3ff9afbfc83a8cdda024973346c7d0789fb
ambroso0 <38117605+ambroso0@users.noreply.github.com>
2018-05-07 10:05:40 +0200

    Update try.sh

--- try.sh
+++ try.sh
@@ -31,4 +31,4 @@ xpx=`echo ${stat}| awk -F ',' '{print $1}'`
 ypx=`echo ${stat}| awk -F ',' '{print $2}'`
 cellmean=`gdalinfo -stats masked_$rast.tif | grep 'STATISTICS_MEAN=' | sed 's/STATISTICS_MEAN=//g' |  sed 's/) (/,/g'`
 
-echo "$cellmean*$xpx*$ypx" >> rast_sum.txt
+echo "$cellmean*$xpx*$ypx" | bc > rast_sum.txt
